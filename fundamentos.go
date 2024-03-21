3d6eb5fb265a8650dc4d74c9d30e5b8a4de54982
Lucas Oliveira <lucasgalvaooliveira12@gmail.com>
2024-03-21 11:59:50 -0400

    mais anotacoes

--- fundamentos.go
+++ fundamentos.go
@@ -1,6 +1,7 @@
 package main // Pacote principal da aplicação
 
 import "fmt" // Pacote externo para print
+import "reflect" // Pacote para descobrir o tipo das variaveis
 
 func main() { // Função da aplicação principal
 
@@ -9,18 +10,28 @@ func main() { // Função da aplicação principal
 	// Tipo String
 	// Quando não for atribuido valor da variavel ele assumira o valor vazio
 	var nome string = "Lucas"
+	nome2 := "Emilly" // Também pode ser declarada dessa forma
 
 	//Tipo Float64 ou Float32 bits para numeros maiores ou menores
 	// Quando não for atribuido valor da variavel ele assumira o valor 0
 	var versao float32 = 1.1
 	var versao2 float64 = 1.232323113131
+	versao3 := 1.223
 
 	// Tipo Inteiro
 	// Quando não for atribuido valor da variavel ele assumira o valor 0
 	var idade int = 18 
+	idade2 := 20
 
-	fmt.Println("Olá, sr. ", nome)
-	fmt.Println("Você tem", idade, "anos")
+	fmt.Println("Olá, sr. ", nome, "sua idade é", idade)
+	fmt.Println("Olá, sr. ", nome2, "sua idade é", idade2)
+
+	
 	fmt.Println("Esse programa está na versão", versao)
 	fmt.Println("A versão extendida desse programa está na", versao2)
+	fmt.Println("A versão extendida desse programa está na", versao3)
+
+
+	fmt.Println("O tipo da variavel nome é", reflect.TypeOf(nome))
+	fmt.Println("O tipo da variavel idade é", reflect.TypeOf(idade))
 }
